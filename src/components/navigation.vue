13255eaddbc0a26b74596d751a75f90269f31560
Elian Caby <54775334+ElianC@users.noreply.github.com>
2023-09-19 10:18:18 +0200

    Initial commit

---
+++ src/components/navigation.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isMenuOpen = ref<boolean>(false)
+
+const openMenu = (action: boolean) => {
+  isMenuOpen.value = action;
+}
+
+const menuLinks = [
+  {
+    name: 'Home',
+    url: '/'
+  },
+  {
+    name: 'About',
+    url: '/about'
+  },
+  {
+    name: 'Blog',
+    url: '/blog'
+  }
+]
+</script>
+
+<template>
+  <div>
+    <nav>
+      <button role="button" @click="()=>openMenu(true)" v-if="!isMenuOpen">
+        Open Menu
+      </button>
+      <button role="button" @click="()=>openMenu(false)" v-else>
+        Close Menu
+      </button>
+      <ul v-if="isMenuOpen">
+        <li v-for="link of menuLinks">
+          <a :href="link.url">{{link.name}}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>

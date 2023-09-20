70ffc71b645374f2291431be844e049abc477532
Elian Caby <54775334+ElianC@users.noreply.github.com>
2023-09-20 12:53:26 +0200

    removing basepath

--- config/astro.ts
+++
@@ -1,1 +0,0 @@
-export const BASE_PATH = "/haraldv2";
--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { BASE_PATH }from '../../config/astro'
 
 const isMenuOpen = ref<boolean>(false)
 
@@ -9,31 +8,26 @@ const openMenu = (action: boolean) => {
 }
 
 const menuLinks = [
-  {
-    name: 'Home',
-    url: `${BASE_PATH}/`,
-  },
+  // {
+  //   name: 'Home',
+  //   url: '/',
+  // },
   {
     name: 'About',
-    url: `${BASE_PATH}/about`,
+    url: '/about',
   },
   {
-    name: 'Blog',
-    url: `${BASE_PATH}/blog`
+    name: 'Photo Gallery',
+    url: '/gallery'
   }
 ]
 </script>
 
 <template>
   <div>
-    <nav>
-      <button role="button" @click="()=>openMenu(true)" v-if="!isMenuOpen">
-        Open Menu
-      </button>
-      <button role="button" @click="()=>openMenu(false)" v-else>
-        Close Menu
-      </button>
-      <ul v-if="isMenuOpen">
+    <nav class="flex justify-between items-end p-4 container mx-auto uppercase">
+      <span class="text-xl ">John Doe</span>
+      <ul v-if="true" class="flex gap-8">
         <li v-for="link of menuLinks">
           <a :href="link.url">{{link.name}}</a>
         </li>

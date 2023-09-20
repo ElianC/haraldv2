6ea908a75058cd4efea04ace291568a9c6f1aeda
Elian Caby <54775334+ElianC@users.noreply.github.com>
2023-09-20 09:37:19 +0200

    adding tailwindCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import vue from "@astrojs/vue";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [vue(), tailwind()],
   site: 'https://eliancaby.github.io',
-  base: '/haraldv2',
+  base: '/haraldv2'
 });

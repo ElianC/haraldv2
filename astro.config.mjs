72ecf2cc1d88ac6b5d75b100b752ab07242add72
Elian Caby <54775334+ElianC@users.noreply.github.com>
2023-12-22 21:47:45 +0100

    removing vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 // import { BASE_PATH }from './config/astro'
 
-import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()],
+  integrations: [tailwind()],
   site: 'https://eliancaby.github.io',
 });
